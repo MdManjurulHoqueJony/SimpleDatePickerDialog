d376ebb024d6e22e22cca9536e280d7f066b42bc
MdManjurulHoqueJony <kucse03jony@gmail.com>
2016-07-20 15:50:08 +0600

    Initial commit

---
+++ app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/simpledatepickerdialog/MainActivity.java
@@ -0,0 +1,49 @@
+package com.wordpress.jonyonandroidcraftsmanship.simpledatepickerdialog;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static TextView SelectedDateView;
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            SelectedDateView.setText("Date of birth(" + day + "/"  + (month + 1) + "/" + year + ")");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SelectedDateView = (TextView) findViewById(R.id.tvDate);
+    }
+
+    public void showDatePickerDialog(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+}
